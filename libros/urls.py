c5c8250a43e541e26d8a055ec0d70a0c754d8ea2
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-03-21 18:15:26 -0500

    Initial commit

--- libros/urls.py
+++ libros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home, crearAutor
+from .views import Home, crearAutor, listAutor, editarAutor
 
 urlpatterns = [
-    path('crear_autor/',crearAutor, name='crear_autor')    
+    path('crear_autor/', crearAutor, name='crearAutor'),
+    path('author_list/', listAutor, name='authorList'),
+    path('autor_edit/<int:id>', editarAutor, name='authorEdit') 
 ]
--- libros/views.py
+++ libros/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import Autorform
+from .models import Autor
 
 def Home(request):
     return render(request,'index.html')
-
 
 def crearAutor(request):
     if request.method == 'POST':
@@ -15,3 +16,22 @@ def crearAutor(request):
     else:
         autor_form=Autorform()
     return render(request,'libros/crear_autor.html',{'autor_form':autor_form})
+
+def listAutor(request):
+    autores = Autor.objects.all()
+    print(autores)
+    return render(request,'libros/autor_list.html',{'autores': autores})
+
+def editarAutor(request, id):
+    try:
+        autor = Autor.objects.get(id = id)          #get is for only one result, when I need information is used method GET, when submit is POST method
+        if request.method == 'GET':
+            autor_form = Autorform(instance = autor)
+        else:
+            autor_form = Autorform(request.POST, instance = autor)
+            if autor_form.is_valid():
+                autor_form.save()
+            return redirect('index')
+    except ObjectDoesNotExist as e:
+        error  = e
+    return render(request, 'libros/crear_autor.html',{'autor_form': autor_form, 'error':error})
